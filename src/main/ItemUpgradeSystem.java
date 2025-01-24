3552f04730b97eb41747a45e5c050448738d11dd
NarekHovhannisyan001 <139554962+NarekHovhannisyan001@users.noreply.github.com>
2025-01-24 18:18:04 +0400

    Create ItemUpgradeSystem.java

---
+++ src/main/ItemUpgradeSystem.java
@@ -0,0 +1,9 @@
+package main;
+
+import ui.ConsoleUI;
+public class ItemUpgradeSystem {
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.run();
+    }
+}
