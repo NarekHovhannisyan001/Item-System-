dcd44150b94b1d014ee611ff5bce90273e681c56
NarekHovhannisyan001 <139554962+NarekHovhannisyan001@users.noreply.github.com>
2025-01-24 18:21:29 +0400

    Add files via upload

---
+++ src/models/Item.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.Objects;
+
+// Base class for Item
+public class Item {
+    private final String name;
+    private Rarity rarity;
+
+    public Item(String name, Rarity rarity) {
+        this.name = name;
+        this.rarity = rarity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Rarity getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(Rarity rarity) {
+        this.rarity = rarity;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Item item = (Item) obj;
+        return Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + rarity + ")";
+    }
+}
---
+++ src/models/Rarity.java
@@ -0,0 +1,6 @@
+package models;
+
+// Enum for item rarity
+public enum Rarity {
+    COMMON, GREAT, RARE, EPIC, LEGENDARY
+}
